81f0c86bd0da5f4e6d712d4e35329dccb73e5e3a
LiewZhanYang <S10259432@connect.np.edu.sg>
2024-02-01 07:36:28 +0800

    doing implement api to python question

--- python quiz/Week-12-Simple-RESTDB/question.js
+++ python quiz/Week-12-Simple-RESTDB/question.js
@@ -16,86 +16,40 @@ let countdown;
 
 //Questions and Options array
 
-const quizArray = [
-  {
-    id: "0",
-    question: "Which is the most widely spoken language in the world?",
-    options: ["Spanish", "Mandarin", "English", "German"],
-    correct: "Mandarin",
-  },
-  {
-    id: "1",
-    question: "Which is the only continent in the world without a desert?",
-    options: ["North America", "Asia", "Africa", "Europe"],
-    correct: "Europe",
-  },
-  {
-    id: "2",
-    question: "Who invented Computer?",
-    options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
-    correct: "Charles Babbage",
-  },
-  {
-    id: "3",
-    question:
-      "What do you call a computer on a network that requests files from another computer?",
-    options: ["A client", "A host", "A router", "A web server"],
-    correct: "A client",
-  },
-  {
-    id: "4",
-    question:
-      "Hardware devices that are not part of the main computer system and are often added later to the system.",
-    options: ["Peripheral", "Clip art", "Highlight", "Execute"],
-    correct: "Peripheral",
-  },
-  {
-    id: "5",
-    question:
-      "The main computer that stores the files that can be sent to computers that are networked together is:",
-    options: ["Clip art", "Mother board", "Peripheral", "File server"],
-    correct: "File server",
-  },
-  {
-    id: "6",
-    question: "How can you catch a computer virus?",
-    options: [
-      "Sending e-mail messages",
-      "Using a laptop during the winter",
-      "Opening e-mail attachments",
-      "Shopping on-line",
-    ],
-    correct: "Opening e-mail attachments",
-  },
-  {
-    id: "7",
-    question: "Google (www.google.com) is a:",
-    options: [
-      "Search Engine",
-      "Number in Math",
-      "Directory of images",
-      "Chat service on the web",
-    ],
-    correct: "Search Engine",
-  },
-  {
-    id: "8",
-    question: "Which is not an Internet protocol?",
-    options: ["HTTP", "FTP", "STP", "IP"],
-    correct: "STP",
-  },
-  {
-    id: "9",
-    question: "Which of the following is not a valid domain name?",
-    options: [
-      "www.yahoo.com",
-      "www.yahoo.co.uk",
-      "www.com.yahoo",
-      "www.yahoo.co.in",
-    ],
-    correct: "www.com.yahoo",
-  },
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const APIKEY = "65b1d9dda2399ae3ac4d588e";
+    const API_URL = "https://fedassignment2ccgame-ef3b.restdb.io/rest/pythonquestion";
+    function initial() {
+        fetch("https://fedassignment2ccgame-ef3b.restdb.io/rest/pythonquestion", {
+          method: "GET",
+          headers: {
+            "x-apikey": "65b1d9dda2399ae3ac4d588e",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          // Convert the fetched data to the expected structure for the quiz
+          const quizArray = data.map(item => ({
+            id: item.id.toString(),
+            question: item.question,
+            options: JSON.parse(item.options), // Assuming options are stored as a JSON string
+            correct: item.answer
+          }));
+      
+          // Proceed with setting up the quiz using the fetched data
+          quizContainer.innerHTML = "";
+          questionCount = 0;
+          scoreCount = 0;
+          count = 11;
+          clearInterval(countdown);
+          timerDisplay();
+          quizCreator(quizArray);  // Pass the quizArray to quizCreator
+          quizDisplay(questionCount);
+        })
+        .catch(error => console.error("Error loading quiz questions:", error));
+      }
+          
 
 //Restart Quiz
 restart.addEventListener("click", () => {
@@ -212,17 +166,6 @@ function checker(userOption) {
   });
 }
 
-//initial setup
-function initial() {
-  quizContainer.innerHTML = "";
-  questionCount = 0;
-  scoreCount = 0;
-  count = 11;
-  clearInterval(countdown);
-  timerDisplay();
-  quizCreator();
-  quizDisplay(questionCount);
-}
 
 //when user click on start button
 startButton.addEventListener("click", () => {
