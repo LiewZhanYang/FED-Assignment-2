874fab3f088a50f26c8ccd7c43b83961e68b5eca
LiewZhanYang <S10259432@connect.np.edu.sg>
2024-02-07 21:43:41 +0800

    doing comment games css

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 "use strict";
 
+// Selecting the navbar element
 const navbar = document.querySelector("[data-navbar]");
+// Selecting all the navbar links
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
+// Selecting the navbar toggler button
 const navbarToggler = document.querySelector("[data-nav-toggler]");
 
+// Adding event listener to the navbar toggler button
 navbarToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
 });
 
+// Looping through each navbar link
 for (let i = 0; i < navbarLinks.length; i++) {
   navbarLinks[i].addEventListener("click", function () {
     navbar.classList.remove("active");
